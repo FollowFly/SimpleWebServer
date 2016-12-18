a81d44df561e2c4973b3859ee3a2803934140cf0
Xiaojinhua <526390171@qq.com>
2016-12-18 16:57:23 +0800

    功能添加

--- server.py
+++ server.py
@@ -17,11 +17,31 @@ class RequestHandler(BaseHTTPRequestHandler):
 </body>
 </html>
 '''
+    Error_Page = """
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
 
     def do_GET(self):
-        full_path = os.getcwd() + self.path
-        page = self.create_page()
-        self.send_page(bytes(page, "utf-8"))
+        try:
+            # Figure out what is exactly being requested
+            full_path = os.getcwd() + self.path
+            if self.path == '/':
+                page = self.create_page()
+                self.send_page(page)
+            elif not os.path.exists(full_path):
+                raise Exception("{0} not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise Exception("{0} unknown error".format(self.path))
+        # Handle errors
+        except Exception as msg:
+            self.handle_error(msg)
 
     def create_page(self):
         values = {
@@ -38,7 +58,26 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(page)
+        self.wfile.write(bytes(page, "utf-8"))
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "{0} can not be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(content)
 
 
 if __name__ == '__main__':
